8879173602693ead3d32125f7939d0f2b3d3004f
Victor S <victor.skarerverket@gmail.com>
2024-04-04 20:57:33 +0200

    saving BishBosh

--- myScript.js
+++ myScript.js
@@ -1,63 +1,37 @@
 document.getElementById("BishBosh").addEventListener("click", BishBosh);
-//document.getElementById("btn").addEventListener("click", Rename);
-//document.getElementById("createDivButton").addEventListener("click", CreateDivs);
-
-function Rename() {
-    document.getElementById("text").innerHTML = "Bnarnar"
-}
-
-function CreateHtml(){
-    var div = document.createElement("div");
-}
-
-function CreateDivs() {
-    //alert("Banana!");
-    for (let i = 0; i < 10; i++) {
-        
-        var div = document.createElement("div");
-        div.innerHTML = "Div " + i;
-
-        document.body.insertBefore(div);
-    }
-}
 
 function BishBosh() {
     
     
     var bish = document.getElementById('SetBish').value;
     var bosh = document.getElementById('SetBosh').value;
+    var loopNr = document.getElementById('SetLoopVal').value;
+    console.log(loopNr);
+    var ul = document.getElementById('BishBoshList');
+    console.log(ul);
 
-    for (let i = 1; i < 100; i++) {
+    for (let i = 1; i <= loopNr; i++) {
+        let li = document.createElement("li");
         if (i % bish == 0 && i % bosh == 0){
             console.log('Bish-Bosh');
+            li.appendChild(document.createTextNode('Bish-Bosh'));
+            ul.appendChild(li);
         }
         else if (i % bish == 0) {
             console.log('Bish');
+            li.appendChild(document.createTextNode('Bish'));
+            ul.appendChild(li);
         }
         else if (i % bosh == 0) {
             console.log('Bosh');
+            li.appendChild(document.createTextNode('Bosh'));
+            ul.appendChild(li);
         } 
         else {
             console.log(i);
+            li.appendChild(document.createTextNode(i));
+            ul.appendChild(li);
         }
     }
 }
-/*
-function BishBosh() {
-    //alert("Banana!");
-    for (let i = 1; i < 100; i++) {
-        if (i % 3 === 0 && i % 4 === 0){
-            console.log('Bish-Bosh');
-        }
-        else if (i % 3 === 0) {
-            console.log('Bish');
-        }
-        else if (i % 4 === 0) {
-            console.log('Bosh');
-        } 
-        else {
-            console.log(i);
-        }
-    }
-}
-*/
+
